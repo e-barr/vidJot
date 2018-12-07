2d643c6fb67a06aa8d434362ea7c72157a5613b9
e-barr <31676765+e-barr@users.noreply.github.com>
2018-12-06 22:52:48 -0600

    update

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
 
+//connect to mongoose
+mongoose.connect('mongodb://localhost/vidjjot-dev', {
+    useNewUrlParser: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err))
 
 //handlebars middleware
 app.engine('handlebars', exphbs({
@@ -27,4 +34,4 @@ const port = 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
-});
+})

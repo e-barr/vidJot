acae1884891f9074b2d04b1cfe6a96ee20302804
e-barr <31676765+e-barr@users.noreply.github.com>
2019-06-06 21:27:01 -0500

    update

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
+
+// map global promise
+mongoose.Promise = global.Promise;
 
 //connect to mongoose
 mongoose.connect('mongodb://localhost/vidjjot-dev', {
     useNewUrlParser: true
 })
 .then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err))
+.catch(err => console.log(err));
+
+// load idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 
 //handlebars middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
-}))
-app.set('view engine', 'handlebars')
+}));
+app.set('view engine', 'handlebars');
 
 //index route
 app.get('/', (req, resp) => {
     const title = 'Welcome'
     resp.render('index', {
         title: title
-    })
-}) 
+    });
+});
 
 //about page
 app.get('/about', (req, resp) => {
     resp.render('about')
-})
+});
 
 const port = 5000;
 
